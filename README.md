77f29227c4ee41a316f410295d8e4ac4bdc0d844
william-index <thewilliamanderson@gmail.com>
2015-08-02 12:29:27 -0400

    pipeline setup for scss and js plus instructions

---
+++ README.md
@@ -0,0 +1,14 @@
+## Pre-reqs
+To run this you must have the following installed:
+- node and npm
+- ruby sass
+- gulp
+
+
+## Setup and Installation
+
+- Clone the Repo
+- Run ```npm i``` form within the repo
+- ```node start``` or ```nodemon``` which I prefer
+- run ```gulp watch```
+- view the site at http://localhost:3000
---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    concat = require('gulp-concat'),
+    wrap = require('gulp-headerfooter');
+
+var sass = require('gulp-ruby-sass'),
+    minifyCss = require('gulp-minify-css');
+
+/*
+ * Sass Pipeline
+ * Compiles and Minimizes Sass
+ */
+gulp.task('sass', function () {
+  return sass('public/src/stylesheets/scss')
+    .pipe(minifyCss())
+    .pipe(gulp.dest('public/css'));
+});
+
+/*
+ * javascript Pipeline
+ * Concats Javascript classes and main into a single file
+ */
+gulp.task('javascript', function () {
+  return gulp.src(['public/src/javascript/classes/*.js',
+                   'public/src/javascript/main.js'])
+    .pipe(concat('main.js', {newLine: ''}))
+    .pipe(wrap.header('(function() {'))
+    .pipe(wrap.footer('})();'))
+    .pipe(gulp.dest('public/js/'));
+});
+
+// Watch Methods
+gulp.task('watch', function () {
+  gulp.watch('public/src/stylesheets/scss/**/*.scss', ['sass']);
+  gulp.watch('public/src/javascript/**/*.js', ['javascript']);
+});
+
+gulp.task('default', ['sass', 'javascript']);
