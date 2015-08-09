ab4bd468f85a99e12df6e233376e9bb9ecf17fd5
william-index <thewilliamanderson@gmail.com>
2015-08-09 12:56:43 -0400

    basic form construction for clothing article submission

--- public/js/main.js
+++ public/js/main.js
@@ -37,6 +37,11 @@ var ClosetManager = (function() {
    */
   var close_picker_btn = document.querySelector('.js-clothing_type_picker .js-close');
 
+  /*
+   * @private {Array<object>}
+   */
+  var clothing_type_btns = document.querySelectorAll('.clothing_type_picker__type');
+
   /**
   * Represents the ClosetManager
   * @constructs ClosetManager
@@ -52,6 +57,8 @@ var ClosetManager = (function() {
   ClosetManager.prototype.bind_events = function(){
     launch_btn.addEventListener('click', this.launch_type_picker);
     close_picker_btn.addEventListener('click', this.dismiss_type_picker);
+    for (var i = 0; i < clothing_type_btns.length; i++) {
+      clothing_type_btns[i].addEventListener('click', this.launch_article_details.bind(this)); }
   }
 
   /**
@@ -69,6 +76,35 @@ var ClosetManager = (function() {
   ClosetManager.prototype.dismiss_type_picker = function(){
     clothing_type_picker.classList.remove("open");
   };
+
+  /**
+  * Sets up and Displays the article details form modal
+  * @param {event<mouse event> e
+  * @private
+  */
+  ClosetManager.prototype.launch_article_details = function(e){
+    var _clothing_type;
+
+    this.dismiss_type_picker();
+
+    if( e.target.hasAttribute("data-type") ) {
+      _clothing_type = e.target.getAttribute("data-type");
+    } else {
+      _clothing_type = e.target.parentNode.getAttribute("data-type");
+    }
+
+    console.log( _clothing_type );
+  };
+
+  /**
+  * Submits form data and upload image for new clothing item
+  * @private
+  */
+  ClosetManager.prototype.submit_clothing_item = function() {
+    // http://howtonode.org/really-simple-file-uploads
+    // http://blog.teamtreehouse.com/uploading-files-ajax
+  };
+
 
   return ClosetManager;
 })();
